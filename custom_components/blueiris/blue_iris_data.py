56e9b6e354d30696dee0b5c7769336aaf02b6858
mjoshd <35782597+mjoshd@users.noreply.github.com>
2019-10-12 05:58:12 -0400

    change camera stream url

--- custom_components/blueiris/blue_iris_data.py
+++ custom_components/blueiris/blue_iris_data.py
@@ -113,7 +113,7 @@ class BlueIrisData:
         self._cast_url = f'{PROTOCOLS[ssl]}://{host}:{port}/mjpg/[CAM_ID]/video.mjpg{cast_credentials}'
 
         self._image_url = f'{self._base_url}/image/[camera_id]?q=100&s=100'
-        self._stream_url = f'{self._base_url}/livestream.htm?cam=[camera_id]'
+        self._stream_url = f'{self.base_url}/h264/[camera_id]/temp.m3u8'
 
     def add_camera(self, camera):
         camera_id = camera.get(CONF_ID)
