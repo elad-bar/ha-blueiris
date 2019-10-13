5c44f69e9b26eefeafbe0a565a82281772c09bc7
Elad Bar <elad.bar@hotmail.com>
2019-10-13 10:20:23 +0300

    Removed override of GenericCamera

--- custom_components/blueiris/camera.py
+++ custom_components/blueiris/camera.py
@@ -6,9 +6,6 @@ https://home-assistant.io/components/camera.blueiris/
 import asyncio
 import logging
 
-import requests
-
-from homeassistant.exceptions import TemplateError
 from homeassistant.const import (CONF_NAME, CONF_USERNAME, CONF_VERIFY_SSL,
                                  CONF_PASSWORD, CONF_AUTHENTICATION)
 from homeassistant.components.camera import (
@@ -56,32 +53,11 @@ def async_setup_platform(hass, config, async_add_entities,
 
         _LOGGER.info(f'Creating camera: {device_info}')
 
-        bi_camera = BlueIrisCamera(hass, device_info)
+        bi_camera = GenericCamera(hass, device_info)
         bi_camera_list.append(bi_camera)
 
         _LOGGER.info(f"Camera created: {bi_camera}")
 
     async_add_entities(bi_camera_list, True)
 
-
-class BlueIrisCamera(GenericCamera):
-    async def async_camera_image(self):
-        """Return a still image response from the camera."""
-        try:
-            url = self._still_image_url.async_render()
-        except TemplateError as err:
-            _LOGGER.error("Error parsing template %s: %s", self._still_image_url, err)
-            return self._last_image
-
-        if url == self._last_url and self._limit_refetch:
-            return self._last_image
-
-        try:
-            response = requests.get(
-                url, timeout=10, auth=self._auth, verify=self.verify_ssl
-            )
-            return response.content
-        except requests.exceptions.RequestException as error:
-            _LOGGER.error("Error getting camera image: %s", error)
-            return self._last_image
 
