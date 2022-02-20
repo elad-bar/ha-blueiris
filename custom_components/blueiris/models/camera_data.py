5b4158d80c86cf8607eba7ea08c6049a0eb49053
kramttocs <11565750+kramttocs@users.noreply.github.com>
2022-02-20 11:09:56 -0600

    Add is_group property to CameraData object
    For possible future use

--- custom_components/blueiris/models/camera_data.py
+++ custom_components/blueiris/models/camera_data.py
@@ -6,6 +6,7 @@ class CameraData:
     name: str
     has_audio: bool
     is_online: bool
+    is_group: bool
     is_system: bool
     data: dict
 
@@ -14,7 +15,8 @@ class CameraData:
         self.name = camera.get(BI_ATTR_NAME)
         self.is_online = camera.get(BI_ATTR_IS_ONLINE, False)
         self.has_audio = camera.get(BI_ATTR_AUDIO, False)
-        self.data = camera
+        self.data = camera		
+        self.is_group = True if(camera.get(BI_ATTR_GROUP) is not None) else False
 
         self.is_system = self.id in SYSTEM_CAMERA_ID
 
@@ -25,6 +27,7 @@ class CameraData:
             CAMERA_HAS_AUDIO: self.has_audio,
             CAMERA_IS_ONLINE: self.is_online,
             CAMERA_IS_SYSTEM: self.is_system,
+            CAMERA_IS_GROUP: self.is_group,
             CAMERA_DATA: self.data,
         }
 
